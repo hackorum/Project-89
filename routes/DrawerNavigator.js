38c7943b8130619b8472fa9c00197abf4c74463a
hackorum <neilsabde@gmail.com>
2021-02-26 20:22:06 +0530

    Drawer Icons

--- routes/DrawerNavigator.js
+++ routes/DrawerNavigator.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { createDrawerNavigator } from "react-navigation-drawer";
 import { TabNavigator } from "./TabNavigator";
 import SettingsScreen from "../screens/SettingsScreen";
@@ -5,6 +6,7 @@ import Sidebar from "../components/Sidebar";
 import MyBarterScreen from "../screens/MyBarterScreen";
 import MyReceivedItemScreen from "../screens/MyReceivedItemScreen";
 import NotificationScreen from "../screens/NotificationScreen";
+import { Icon } from "react-native-elements";
 
 export const DrawerNavigator = createDrawerNavigator(
   {
@@ -12,27 +14,34 @@ export const DrawerNavigator = createDrawerNavigator(
       screen: TabNavigator,
       navigationOptions: {
         title: "Home",
+        drawerIcon: <Icon name="home" type="fontawesome5" />,
       },
     },
     Settings: {
       screen: SettingsScreen,
+      navigationOptions: {
+        drawerIcon: <Icon name="gear" type="font-awesome" />,
+      },
     },
     MyBarters: {
       screen: MyBarterScreen,
       navigationOptions: {
         title: "My Barters",
+        drawerIcon: <Icon name="gift" type="font-awesome" />,
       },
     },
     MyReceivedItemScreen: {
       screen: MyReceivedItemScreen,
       navigationOptions: {
         title: "Received Items",
+        drawerIcon: <Icon name="book" type="font-awesome" />,
       },
     },
     Notifications: {
       screen: NotificationScreen,
       navigationOptions: {
         title: "Notifications",
+        drawerIcon: <Icon name="bell" type="font-awesome" />,
       },
     },
   },
